3494201ea9e194e685c0028513a21f6d5f2aae7e
mathgvlt0 <mgayvallet@edenschool.fr>
2023-12-18 17:12:05 +0100

    projet

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./*.js",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-mobile": "url('images/homepage/hero-bg-mobile.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
